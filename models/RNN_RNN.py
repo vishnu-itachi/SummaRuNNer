a8b33c6edb8ce0cd1ff2ed52e93bdf539e28b57d
“vishnu-itachi” <vishnu14july@gmail.com>
2021-05-22 09:58:45 +0000

    “dataloader”

--- models/RNN_RNN.py
+++ models/RNN_RNN.py
@@ -1,15 +1,21 @@
+# SummaRuNNer with BERT model/RNN_RNN.py
 from .BasicModule import BasicModule
+import subprocess as sp
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+from transformers import BertTokenizer, BertModel
+
+HH = 256
 
 class RNN_RNN(BasicModule):
     def __init__(self, args, embed=None):
         super(RNN_RNN, self).__init__(args)
         self.model_name = 'RNN_RNN'
         self.args = args
-        
+
         V = args.embed_num
         D = args.embed_dim
         H = args.hidden_size
@@ -22,24 +28,24 @@ class RNN_RNN(BasicModule):
         if embed is not None:
             self.embed.weight.data.copy_(embed)
 
-        self.word_RNN = nn.GRU(
-                        input_size = D,
-                        hidden_size = H,
-                        batch_first = True,
-                        bidirectional = True
-                        )
+        self.bert_m = BertModel.from_pretrained('bert-base-cased',
+                                  ) 
+        
+        for name, param in list(self.bert_m.named_parameters())[:-66]:
+            param.requires_grad = False
+
         self.sent_RNN = nn.GRU(
-                        input_size = 2*H,
-                        hidden_size = H,
+                        input_size = 768,
+                        hidden_size = HH,
                         batch_first = True,
                         bidirectional = True
                         )
-        self.fc = nn.Linear(2*H,2*H)
+        self.fc = nn.Linear(2*HH,2*HH)
 
         # Parameters of Classification Layer
-        self.content = nn.Linear(2*H,1,bias=False)
-        self.salience = nn.Bilinear(2*H,2*H,1,bias=False)
-        self.novelty = nn.Bilinear(2*H,2*H,1,bias=False)
+        self.content = nn.Linear(2*HH,1,bias=False)
+        self.salience = nn.Bilinear(2*HH,2*HH,1,bias=False)
+        self.novelty = nn.Bilinear(2*HH,2*HH,1,bias=False)
         self.abs_pos = nn.Linear(P_D,1,bias=False)
         self.rel_pos = nn.Linear(P_D,1,bias=False)
         self.bias = nn.Parameter(torch.FloatTensor(1).uniform_(-0.1,0.1))
@@ -51,9 +57,10 @@ class RNN_RNN(BasicModule):
             t = t[:seq_lens[index],:]
             t = torch.t(t).unsqueeze(0)
             out.append(F.max_pool1d(t,t.size(2)))
-        
+
         out = torch.cat(out).squeeze(2)
         return out
+
     def avg_pool1d(self,x,seq_lens):
         # x:[N,L,O_in]
         out = []
@@ -61,29 +68,45 @@ class RNN_RNN(BasicModule):
             t = t[:seq_lens[index],:]
             t = torch.t(t).unsqueeze(0)
             out.append(F.avg_pool1d(t,t.size(2)))
-        
+
         out = torch.cat(out).squeeze(2)
         return out
-    def forward(self,x,doc_lens):
-        sent_lens = torch.sum(torch.sign(x),dim=1).data 
-        x = self.embed(x)                                                      # (N,L,D)
+
+    def forward(self,input_ids,attention_masks,doc_lens):
         # word level GRU
         H = self.args.hidden_size
-        x = self.word_RNN(x)[0]                                                 # (N,2*H,L)
-        #word_out = self.avg_pool1d(x,sent_lens)
-        word_out = self.max_pool1d(x,sent_lens)
-        # make sent features(pad with zeros)
-        x = self.pad_doc(word_out,doc_lens)
-
-        # sent level GRU
-        sent_out = self.sent_RNN(x)[0]                                           # (B,max_doc_len,2*H)
-        #docs = self.avg_pool1d(sent_out,doc_lens)                               # (B,2*H)
-        docs = self.max_pool1d(sent_out,doc_lens)                                # (B,2*H)
+        outputs = self.bert_m(input_ids=input_ids, attention_mask=attention_masks)
+        # hidden representation of last layer 
+        token_vecs = outputs.last_hidden_state
+        # dimension : [N,max_len_sent,768] N: no of sentences
+        k=0
+        for i in token_vecs:
+            # cls embedding
+            sentence_embedding = i[0] 
+            if(k==0): 
+                sen = sentence_embedding.unsqueeze(0) 
+                emb = sen
+                k=k+1
+            else:
+                sen = sentence_embedding.unsqueeze(0)
+                emb = torch.cat((emb,sen),0)
+        
+        torch.cuda.empty_cache()
+        k=0
+        x = self.pad_doc(emb,doc_lens)
+        sent_out = self.sent_RNN(x)[0]
+        docs = self.max_pool1d(sent_out,doc_lens)
+        del emb
+        torch.cuda.empty_cache()
+        del input_ids
+        del attention_masks
+        torch.cuda.empty_cache()
         probs = []
+        
         for index,doc_len in enumerate(doc_lens):
             valid_hidden = sent_out[index,:doc_len,:]                            # (doc_len,2*H)
             doc = F.tanh(self.fc(docs[index])).unsqueeze(0)
-            s = Variable(torch.zeros(1,2*H))
+            s = Variable(torch.zeros(1,2*HH))
             if self.args.device is not None:
                 s = s.cuda()
             for position, h in enumerate(valid_hidden):
@@ -93,15 +116,15 @@ class RNN_RNN(BasicModule):
                 if self.args.device is not None:
                     abs_index = abs_index.cuda()
                 abs_features = self.abs_pos_embed(abs_index).squeeze(0)
-                
+
                 rel_index = int(round((position + 1) * 9.0 / doc_len))
                 rel_index = Variable(torch.LongTensor([[rel_index]]))
                 if self.args.device is not None:
                     rel_index = rel_index.cuda()
                 rel_features = self.rel_pos_embed(rel_index).squeeze(0)
-                
+
                 # classification layer
-                content = self.content(h) 
+                content = self.content(h)
                 salience = self.salience(h,doc)
                 novelty = -1 * self.novelty(h,F.tanh(s))
                 abs_p = self.abs_pos(abs_features)
@@ -109,4 +132,7 @@ class RNN_RNN(BasicModule):
                 prob = F.sigmoid(content + salience + novelty + abs_p + rel_p + self.bias)
                 s = s + torch.mm(prob,h)
                 probs.append(prob)
+        del sent_out
+        del docs
+        torch.cuda.empty_cache()
         return torch.cat(probs).squeeze()
--- utils/Vocab.py
+++ utils/Vocab.py
@@ -1,4 +1,8 @@
+# SummaRuNNer with BERT utils/Vocab.py
+
 import torch
+from transformers import BertTokenizer, BertModel
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
 class Vocab():
     def __init__(self,embed,word2id):
@@ -10,7 +14,7 @@ class Vocab():
         self.UNK_IDX = 1
         self.PAD_TOKEN = 'PAD_TOKEN'
         self.UNK_TOKEN = 'UNK_TOKEN'
-    
+
     def __len__(self):
         return len(word2id)
 
@@ -21,10 +25,11 @@ class Vocab():
             return self.word2id[w]
         else:
             return self.UNK_IDX
-    
-    def make_features(self,batch,sent_trunc=50,doc_trunc=100,split_token='\n'):
+
+    def make_features(self,batch,sent_trunc=32,doc_trunc=800,split_token='\n'):
         sents_list,targets,doc_lens = [],[],[]
         # trunc document
+        # print(batch)
         for doc,label in zip(batch['doc'],batch['labels']):
             sents = doc.split(split_token)
             labels = label.split(split_token)
@@ -44,19 +49,35 @@ class Vocab():
                 words = words[:sent_trunc]
             max_sent_len = len(words) if len(words) > max_sent_len else max_sent_len
             batch_sents.append(words)
-        
-        features = []
-        for sent in batch_sents:
-            feature = [self.w2i(w) for w in sent] + [self.PAD_IDX for _ in range(max_sent_len-len(sent))]
-            features.append(feature)
-        
-        features = torch.LongTensor(features)    
+
+        input_ids = []
+        attention_masks = []
+        for doc in batch['doc']:
+            doc_n = doc.split(split_token)
+            k=0
+            for sent in doc_n:
+                encoded_dict = tokenizer.encode_plus(
+                    sent,
+                    None,
+                    add_special_tokens=True,
+                    max_length=64,
+                    pad_to_max_length=True,
+                    return_token_type_ids=True,
+                    return_tensors="pt"
+                    )
+                k=k+1
+                input_ids.append(encoded_dict['input_ids'])
+                attention_masks.append(encoded_dict['attention_mask'])
+                if(k==doc_trunc):
+                    break
+        input_ids = torch.cat(input_ids, dim=0)
+        attention_masks = torch.cat(attention_masks, dim=0)
         targets = torch.LongTensor(targets)
         summaries = batch['summaries']
 
-        return features,targets,summaries,doc_lens
+        return input_ids,attention_masks,targets,summaries,doc_lens
 
-    def make_predict_features(self, batch, sent_trunc=150, doc_trunc=100, split_token='. '):
+    def make_predict_features(self, batch, sent_trunc=150, doc_trunc=300, split_token='. '):
         sents_list, doc_lens = [],[]
         for doc in batch:
             sents = doc.split(split_token)
@@ -79,6 +100,22 @@ class Vocab():
             feature = [self.w2i(w) for w in sent] + [self.PAD_IDX for _ in range(max_sent_len-len(sent))]
             features.append(feature)
 
-        features = torch.LongTensor(features)
+        #features = torch.LongTensor(features)
+        input_ids = []
+        attention_masks = []
+        for sent in sents_list:
+            encoded_dict = tokenizer.encode_plus(
+                    sent,                      # Sentence to encode.
+                    add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                    max_length = 128,           # Pad & truncate all sentences.
+                    truncation = True,
+                    pad_to_max_length = True,
+                    return_attention_mask = True,   # Construct attn. masks.
+                    return_tensors = 'pt',     # Return pytorch tensors.
+                 )
+            input_ids.append(encoded_dict['input_ids'])
+            attention_masks.append(encoded_dict['attention_mask'])
+        input_ids = torch.cat(input_ids, dim=0)
+        attention_masks = torch.cat(attention_masks, dim=0)
 
-        return features, doc_lens
+        return  input_ids,attention_masks,doc_lens
