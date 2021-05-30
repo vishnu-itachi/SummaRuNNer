683985cb93446934822b56a127752183f16743ff
vishnu <vishnu14july@gmail.com>
2021-05-30 14:18:12 +0530

    added mtl

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ from transformers import BertTokenizer, BertModel
 from torch.nn.utils import clip_grad_norm
 from time import time
 from tqdm import tqdm
-from collections import namedtuple
 import pandas as pd
 from pprint import pprint
 
@@ -72,11 +71,11 @@ args = parser.parse_args()
 use_gpu = args.device is not None
 
 if torch.cuda.is_available() and not use_gpu:
-    print("WARNING: You have a CUDA device, should run with -device 0")
+	print("WARNING: You have a CUDA device, should run with -device 0")
 
 # set cuda device and seed
 if use_gpu:
-    torch.cuda.set_device(args.device)
+	torch.cuda.set_device(args.device)
 torch.cuda.manual_seed(args.seed)
 torch.manual_seed(args.seed)
 random.seed(args.seed)
@@ -84,269 +83,273 @@ numpy.random.seed(args.seed)
 
 
 def eval(net,vocab,data_iter,criterion):
-    with torch.no_grad():
-        net.eval()
-        total_loss = 0
-        batch_num = 0
-        for batch in data_iter:
-            input_ids,attention_masks,targets,_,doc_lens = vocab.make_features(batch)
-            input_ids,attention_masks,targets = Variable(input_ids),Variable(attention_masks), Variable(targets.float())
-            if use_gpu:
-                targets = targets.cuda()
-                input_ids = input_ids.cuda()
-                attention_masks = attention_masks.cuda()
-            probs = net(input_ids,attention_masks,doc_lens)
-            loss = criterion(probs,targets)
-            total_loss += loss.item()
-            batch_num += 1
-        loss = total_loss / batch_num
-        del targets
-        del input_ids
-        del attention_masks
-        torch.cuda.empty_cache()
-        net.train()
-    return loss
+	with torch.no_grad():
+		net.eval()
+		total_loss = 0
+		batch_num = 0
+		for batch in data_iter:
+			input_ids,attention_masks,targets,_,doc_lens = vocab.make_features(batch)
+			input_ids,attention_masks,targets = Variable(input_ids),Variable(attention_masks), Variable(targets.float())
+			if use_gpu:
+				targets = targets.cuda()
+				input_ids = input_ids.cuda()
+				attention_masks = attention_masks.cuda()
+			probs = net(input_ids,attention_masks,doc_lens)
+			loss = criterion(probs,targets)
+			total_loss += loss.item()
+			batch_num += 1
+		loss = total_loss / batch_num
+		del targets
+		del input_ids
+		del attention_masks
+		torch.cuda.empty_cache()
+		net.train()
+	return loss
 
 def train():
-    logging.info('Loading vocab,train and val dataset.Wait a second,please')
-    pp = 3
-    embed = torch.Tensor(np.load(args.embedding)['embedding'])
-    with open(args.word2id) as f:
-        word2id = json.load(f)
-    vocab = utils.Vocab(embed, word2id)
+	logging.info('Loading vocab,train and val dataset.Wait a second,please')
+	pp = 3
+	embed = torch.Tensor(np.load(args.embedding)['embedding'])
+	with open(args.word2id) as f:
+		word2id = json.load(f)
+	vocab = utils.Vocab(embed, word2id)
 
-    with open(args.train_dir) as f:
-        examples = json.load(f)
-    # print(len(examples))
-    train_dataset = utils.Dataset(examples)
-    # print(train_dataset)
-    with open(args.val_dir) as f:
-        examples = json.load(f)
-    val_dataset = utils.Dataset(examples)
+	with open(args.train_dir) as f:
+		examples = json.load(f)
+	# print(len(examples))
+	train_dataset = utils.Dataset(examples)
+	# print(train_dataset)
+	with open(args.val_dir) as f:
+		examples = json.load(f)
+	val_dataset = utils.Dataset(examples)
 
-    # update args
-    args.embed_num = embed.size(0)
-    args.embed_dim = embed.size(1)
-    args.kernel_sizes = [int(ks) for ks in args.kernel_sizes.split(',')]
-    acc_steps = 16
-    # build model
-    net = getattr(models,args.model)(args,embed)
-    if use_gpu:
-        net.cuda()
-    # load dataset
-    train_iter = DataLoader(dataset=train_dataset,
-            batch_size=args.batch_size,
-            shuffle=True)
-    val_iter = DataLoader(dataset=val_dataset,
-            batch_size=args.batch_size,
-            shuffle=False)
-    # loss function
-    criterion = nn.BCELoss()
-    # model info
-    print(net)
-    params = sum(p.numel() for p in list(net.parameters())) / 1e6
-    print('#Params: %.1fM' % (params))
+	# update args
+	args.embed_num = embed.size(0)
+	args.embed_dim = embed.size(1)
+	args.kernel_sizes = [int(ks) for ks in args.kernel_sizes.split(',')]
+	acc_steps = 16
+	# build model
+	net = getattr(models,args.model)(args,embed)
+	if use_gpu:
+		net.cuda()
+	# load dataset
+	train_iter = DataLoader(dataset=train_dataset,
+			batch_size=args.batch_size,
+			shuffle=True)
+	val_iter = DataLoader(dataset=val_dataset,
+			batch_size=args.batch_size,
+			shuffle=False)
+	# loss function
+	criterion = nn.BCELoss()
+	# model info
+	print(net)
+	params = sum(p.numel() for p in list(net.parameters())) / 1e6
+	print('#Params: %.1fM' % (params))
 
-    min_loss = float('inf')
-    optimizer = torch.optim.Adam(net.parameters(),lr=args.lr)
-    net.train()
+	min_loss = float('inf')
+	optimizer = torch.optim.Adam(net.parameters(),lr=args.lr)
+	net.train()
 
-    t1 = time()
-    checkpp = 0
-    for epoch in range(1,args.epochs+1):
-        if(checkpp==pp):
-            break
-        optimizer.zero_grad()
-        t_loss = 0
-        s_loss = 0
-        from pprint import pprint
-        for i,batch in enumerate(train_iter):
-            # print(batch)
-            input_ids,attention_masks,targets,_,doc_lens = vocab.make_features(batch)
-            input_ids,attention_masks,targets = Variable(input_ids),Variable(attention_masks), Variable(targets.float())
-            if use_gpu:
-                input_ids = input_ids.cuda()
-                attention_masks = attention_masks.cuda()
-               
-            probs = net(input_ids,attention_masks,doc_lens)
-            if use_gpu:
-                targets = targets.cuda()
-           
-            loss = criterion(probs,targets)
-            t_loss = t_loss+loss.item()
-            loss = loss / acc_steps
-            s_loss = s_loss+1
-            loss.backward()
-            clip_grad_norm(net.parameters(), args.max_norm)
-            if(((i+1) % acc_steps == 0) or (i==800)):
-                optimizer.step()
-                optimizer.zero_grad()
-            if args.debug:
-                print('Batch ID:%d Loss:%f' %(i,loss.data[0]))
-                continue
-            if( (i % args.report_every == 0) and (i!=0)):
-                cur_loss = eval(net,vocab,val_iter,criterion)
-                train_loss = t_loss/s_loss
-                t_loss = 0
-                s_loss = 0
-                if cur_loss < min_loss:
-                    checkpp = 0
-                    min_loss = cur_loss
-                    best_path = net.save()
-                else:
-                    checkpp = checkpp+1
+	t1 = time()
+	checkpp = 0
+	for epoch in range(1,args.epochs+1):
+		if(checkpp==pp):
+			break
+		optimizer.zero_grad()
+		t_loss = 0
+		s_loss = 0
+		from pprint import pprint
+		for i,batch in enumerate(train_iter):
+			# print(batch)
+			input_ids,attention_masks,targets,_,doc_lens = vocab.make_features(batch)
+			input_ids,attention_masks,targets = Variable(input_ids),Variable(attention_masks), Variable(targets.float())
+			if use_gpu:
+				input_ids = input_ids.cuda()
+				attention_masks = attention_masks.cuda()
+			   
+			probs = net(input_ids,attention_masks,doc_lens)
+			if use_gpu:
+				targets = targets.cuda()
+		   
+			loss = criterion(probs,targets)
+			t_loss = t_loss+loss.item()
+			loss = loss / acc_steps
+			s_loss = s_loss+1
+			loss.backward()
+			clip_grad_norm(net.parameters(), args.max_norm)
+			if(((i+1) % acc_steps == 0) or (i==800)):
+				optimizer.step()
+				optimizer.zero_grad()
+			if args.debug:
+				print('Batch ID:%d Loss:%f' %(i,loss.data[0]))
+				continue
+			if( (i % args.report_every == 0) and (i!=0)):
+				cur_loss = eval(net,vocab,val_iter,criterion)
+				train_loss = t_loss/s_loss
+				t_loss = 0
+				s_loss = 0
+				if cur_loss < min_loss:
+					checkpp = 0
+					min_loss = cur_loss
+					best_path = net.save()
+				else:
+					checkpp = checkpp+1
 
-                logging.info('Epoch: %2d Min_Val_Loss: %f Cur_Val_Loss: %f training loss: %f'
-                        % (epoch,min_loss,cur_loss,train_loss))
+				logging.info('Epoch: %2d Min_Val_Loss: %f Cur_Val_Loss: %f training loss: %f'
+						% (epoch,min_loss,cur_loss,train_loss))
 
-    t2 = time()
-    logging.info('Total Cost:%f h'%((t2-t1)/3600))
+	t2 = time()
+	logging.info('Total Cost:%f h'%((t2-t1)/3600))
 
+
+'''
 def Get_tweetID(sentence):
-    # same tweet content different IDs
-    x = dfmain[dfmain['Clean_Tweet']==sentence].Tweet_ID.iloc[0]
-    # print(x)
-    return int(x)
+	# same tweet content different IDs
+	x = dfmain[dfmain['Clean_Tweet']==sentence].Tweet_ID.iloc[0]
+	# print(x)
+	return int(x)
+'''
 
-def test():
-    aggregate_dict = {int(tweetd):{'sum':0,'count':0} for tweetd in dfs.Tweet_ID}
-    embed = torch.Tensor(np.load(args.embedding)['embedding'])
-    with open(args.word2id) as f:
-        word2id = json.load(f)
-    vocab = utils.Vocab(embed, word2id)
 
-    with open(args.test_dir) as f:
-        # examples = [json.loads(line) for line in f]
-        examples = json.load(f)
-    test_dataset = utils.Dataset(examples)
+def test(dfp):
+	aggregate_dict = {int(tweetd):{'sum':0,'count':0} for tweetd in dfp.Tweet_ID}
+	embed = torch.Tensor(np.load(args.embedding)['embedding'])
+	with open(args.word2id) as f:
+		word2id = json.load(f)
+	vocab = utils.Vocab(embed, word2id)
 
-    test_iter = DataLoader(dataset=test_dataset,
-                            batch_size=args.batch_size,
-                            shuffle=False)
-    if use_gpu:
-        checkpoint = torch.load(args.load_dir)
-    else:
-        checkpoint = torch.load(args.load_dir, map_location=lambda storage, loc: storage)
+	with open(args.test_dir) as f:
+		# examples = [json.loads(line) for line in f]
+		examples = json.load(f)
+	test_dataset = utils.Dataset(examples)
 
-    # checkpoint['args']['device'] saves the device used as train time
-    # if at test time, we are using a CPU, we must override device to None
-    if not use_gpu:
-        checkpoint['args'].device = None
-    net = getattr(models,checkpoint['args'].model)(checkpoint['args'])
-    net.load_state_dict(checkpoint['model'])
-    if use_gpu:
-        net.cuda()
-    net.eval()
+	test_iter = DataLoader(dataset=test_dataset,
+							batch_size=args.batch_size,
+							shuffle=False)
+	if use_gpu:
+		checkpoint = torch.load(args.load_dir)
+	else:
+		checkpoint = torch.load(args.load_dir, map_location=lambda storage, loc: storage)
 
-    doc_num = len(test_dataset)
-    time_cost = 0
-    file_id = 1
-    for batch in tqdm(test_iter):
-        input_ids,attention_masks,targets,summaries,doc_lens  = vocab.make_features(batch)
-        input_ids,attention_masks,targets = Variable(input_ids),Variable(attention_masks), Variable(targets.float())
-        t1 = time()
-        if use_gpu:
-            input_ids = input_ids.cuda()
-            attention_masks = attention_masks.cuda()
+	# checkpoint['args']['device'] saves the device used as train time
+	# if at test time, we are using a CPU, we must override device to None
+	if not use_gpu:
+		checkpoint['args'].device = None
+	net = getattr(models,checkpoint['args'].model)(checkpoint['args'])
+	net.load_state_dict(checkpoint['model'])
+	if use_gpu:
+		net.cuda()
+	net.eval()
 
-            probs = net(input_ids,attention_masks,doc_lens)
-        else:
-            probs = net(input_ids,attention_masks,doc_lens)
-        t2 = time()
-        time_cost += t2 - t1
-        start = 0
-        for doc_id,doc_len in enumerate(doc_lens):
-            # print(doc_len)
-            stop = start + doc_len
-            prob = probs[start:stop]
-            topk = min(args.topk,doc_len)
-            topk = doc_len
-            topk_indices = prob.topk(topk)[1].cpu().data.numpy()
-            # topk_indices.sort()
-            doc = batch['doc'][doc_id].split('\n')[:doc_len]
-            tweeidlist = batch['tweetid'][doc_id].split('\n')[:doc_len]
-            for index in topk_indices:
-                sentence = doc[index]
-                # tid = Get_tweetID(sentence)
-                tid = int(tweeidlist[index])
-                aggregate_dict[tid]['sum']+=prob[index].item()
-                aggregate_dict[tid]['count']+=1
-            hyp = [str(prob[index].item()) + "\t"+ doc[index] for index in topk_indices]
-            ref = summaries[doc_id]
-            with open(os.path.join(args.ref,str(file_id)+'.txt'), 'w') as f:
-                f.write(ref)
-            with open(os.path.join(args.hyp,str(file_id)+'.txt'), 'w') as f:
-                f.write('\n'.join(hyp))
-            start = stop
-            file_id = file_id + 1
-        del input_ids
-        del attention_masks
-        torch.cuda.empty_cache()
+	doc_num = len(test_dataset)
+	time_cost = 0
+	file_id = 1
+	for batch in tqdm(test_iter):
+		input_ids,attention_masks,targets,summaries,doc_lens  = vocab.make_features(batch)
+		input_ids,attention_masks,targets = Variable(input_ids),Variable(attention_masks), Variable(targets.float())
+		t1 = time()
+		if use_gpu:
+			input_ids = input_ids.cuda()
+			attention_masks = attention_masks.cuda()
 
-    pprint(aggregate_dict)
-    print('Speed: %.2f docs / s' % (doc_num / time_cost))
+			probs = net(input_ids,attention_masks,doc_lens)
+		else:
+			probs = net(input_ids,attention_masks,doc_lens)
+		t2 = time()
+		time_cost += t2 - t1
+		start = 0
+		for doc_id,doc_len in enumerate(doc_lens):
+			# print(doc_len)
+			stop = start + doc_len
+			prob = probs[start:stop]
+			topk = min(args.topk,doc_len)
+			topk = doc_len
+			topk_indices = prob.topk(topk)[1].cpu().data.numpy()
+			# topk_indices.sort()
+			doc = batch['doc'][doc_id].split('\n')[:doc_len]
+			tweeidlist = batch['tweetid'][doc_id].split('\n')[:doc_len]
+			for index in topk_indices:
+				sentence = doc[index]
+				# tid = Get_tweetID(sentence)
+				tid = int(tweeidlist[index])
+				aggregate_dict[tid]['sum']+=prob[index].item()
+				aggregate_dict[tid]['count']+=1
+			hyp = [str(prob[index].item()) + "\t"+ doc[index] for index in topk_indices]
+			ref = summaries[doc_id]
+			with open(os.path.join(args.ref,str(file_id)+'.txt'), 'w') as f:
+				f.write(ref)
+			with open(os.path.join(args.hyp,str(file_id)+'.txt'), 'w') as f:
+				f.write('\n'.join(hyp))
+			start = stop
+			file_id = file_id + 1
+		del input_ids
+		del attention_masks
+		torch.cuda.empty_cache()
+
+	pprint(aggregate_dict)
+	print('Speed: %.2f docs / s' % (doc_num / time_cost))
 
 
 def predict(examples):
-    embed = torch.Tensor(np.load(args.embedding)['embedding'])
-    with open(args.word2id) as f:
-        word2id = json.load(f)
-    vocab = utils.Vocab(embed, word2id)
-    pred_dataset = utils.Dataset(examples)
+	embed = torch.Tensor(np.load(args.embedding)['embedding'])
+	with open(args.word2id) as f:
+		word2id = json.load(f)
+	vocab = utils.Vocab(embed, word2id)
+	pred_dataset = utils.Dataset(examples)
 
-    pred_iter = DataLoader(dataset=pred_dataset,
-                            batch_size=args.batch_size,
-                            shuffle=False)
-    if use_gpu:
-        checkpoint = torch.load(args.load_dir)
-    else:
-        checkpoint = torch.load(args.load_dir, map_location=lambda storage, loc: storage)
+	pred_iter = DataLoader(dataset=pred_dataset,
+							batch_size=args.batch_size,
+							shuffle=False)
+	if use_gpu:
+		checkpoint = torch.load(args.load_dir)
+	else:
+		checkpoint = torch.load(args.load_dir, map_location=lambda storage, loc: storage)
 
-    # checkpoint['args']['device'] saves the device used as train time
-    # if at test time, we are using a CPU, we must override device to None
-    if not use_gpu:
-        checkpoint['args'].device = None
-    net = getattr(models,checkpoint['args'].model)(checkpoint['args'])
-    net.load_state_dict(checkpoint['model'])
-    if use_gpu:
-        net.cuda()
-    net.eval()
+	# checkpoint['args']['device'] saves the device used as train time
+	# if at test time, we are using a CPU, we must override device to None
+	if not use_gpu:
+		checkpoint['args'].device = None
+	net = getattr(models,checkpoint['args'].model)(checkpoint['args'])
+	net.load_state_dict(checkpoint['model'])
+	if use_gpu:
+		net.cuda()
+	net.eval()
 
-    doc_num = len(pred_dataset)
-    time_cost = 0
-    file_id = 1
-    for batch in tqdm(pred_iter):
-        input_ids,attention_masks, doc_lens = vocab.make_predict_features(batch)
-        t1 = time()
-        if use_gpu:
-            probs = net(input_ids,attention_masks,doc_lens)
-        else:
-            input_ids,attention_masks = Variable(input_ids),Variable(attention_masks)
-            probs = net(input_ids,attention_masks, doc_lens)
-        t2 = time()
-        time_cost += t2 - t1
-        start = 0
-        for doc_id,doc_len in enumerate(doc_lens):
-            stop = start + doc_len
-            prob = probs[start:stop]
-            topk = min(args.topk,doc_len)
-            topk_indices = prob.topk(topk)[1].cpu().data.numpy()
-            topk_indices.sort()
-            
-            doc = batch[doc_id].split('. ')[:doc_len]
-            hyp = [doc[index] for index in topk_indices]
-            with open(os.path.join(args.hyp,str(file_id)+'.txt'), 'w') as f:
-                f.write('. '.join(hyp))
-            start = stop
-            file_id = file_id + 1
-    print('Speed: %.2f docs / s' % (doc_num / time_cost))
+	doc_num = len(pred_dataset)
+	time_cost = 0
+	file_id = 1
+	for batch in tqdm(pred_iter):
+		input_ids,attention_masks, doc_lens = vocab.make_predict_features(batch)
+		t1 = time()
+		if use_gpu:
+			probs = net(input_ids,attention_masks,doc_lens)
+		else:
+			input_ids,attention_masks = Variable(input_ids),Variable(attention_masks)
+			probs = net(input_ids,attention_masks, doc_lens)
+		t2 = time()
+		time_cost += t2 - t1
+		start = 0
+		for doc_id,doc_len in enumerate(doc_lens):
+			stop = start + doc_len
+			prob = probs[start:stop]
+			topk = min(args.topk,doc_len)
+			topk_indices = prob.topk(topk)[1].cpu().data.numpy()
+			topk_indices.sort()
+			
+			doc = batch[doc_id].split('. ')[:doc_len]
+			hyp = [doc[index] for index in topk_indices]
+			with open(os.path.join(args.hyp,str(file_id)+'.txt'), 'w') as f:
+				f.write('. '.join(hyp))
+			start = stop
+			file_id = file_id + 1
+	print('Speed: %.2f docs / s' % (doc_num / time_cost))
 
 if __name__=='__main__':
-    if args.test:
-        test()
-    elif args.predict:
-        with open(args.filename) as file:
-            bod = [file.read()]
-        predict(bod)
-    else:
-        train()
+	if args.test:
+		test()
+	elif args.predict:
+		with open(args.filename) as file:
+			bod = [file.read()]
+		predict(bod)
+	else:
+		train()
--- utils/Dataset.py
+++ utils/Dataset.py
@@ -4,6 +4,8 @@ import torch.utils.data as data
 from torch.autograd import Variable
 from .Vocab import Vocab
 import numpy as np
+import math, random
+import pandas as pd
 
 class Dataset(data.Dataset):
     def __init__(self, examples):
@@ -42,3 +44,55 @@ class Dataset(data.Dataset):
         
     def __len__(self):
         return len(self.examples)
+
+
+class Place:
+    def __init__(self,df,replicate=5):
+        self.df = df
+        self.replicate = replicate-1
+        self.tweetidlist = random.sample(list(df.Tweet_ID), k=len(df))
+    def __len__(self):
+        return (self.replicate + 1)*len(self.df)
+
+    def get(self, docsize):
+        if not self.tweetidlist and not self.replicate:
+            return None
+            # raise Exception("Tweets exhausted")
+        tweetidlist,self.tweetidlist = self.tweetidlist[:docsize],self.tweetidlist[docsize:]
+        if not self.tweetidlist and self.replicate:
+            self.tweetidlist = random.sample(list(self.df.Tweet_ID), k=len(self.df))
+            self.replicate-=1
+        df = self.df[self.df["Tweet_ID"].isin(tweetidlist)]
+        df = df.sort_values("Date", ignore_index=True)
+        return df
+    
+class Custom_Dataset(Dataset):
+    def __init__(self, placelist, docsize):
+        self.placelist = placelist
+        self.docsize = docsize
+
+    def __len__(self):
+        s = sum([math.ceil(len(p)/self.docsize) for p in placelist])
+        return s
+    
+    def __getitem__(self, index):
+        place = random.choice(self.placelist)
+        df = place.get(self.docsize)
+        while df is None:
+            self.placelist.remove(place)
+            place = random.choice(self.placelist)
+            df = place.get(self.docsize)
+        tweetid = "\n".join(map(str,list(df.Tweet_ID)))
+        doc = "\n".join(list(df.Clean_Tweet))
+        labels = "\n".join(map(str,list(df.New_Summ_gt_Clean)))
+        summaries = "None"
+        # dfdoc = pd.DataFrame({'doc':doc,'labels':labels,'summaries':summaries})
+        return (tweetid, doc, labels,summaries)
+
+def createplacelist(traindflist, testdf):
+    maxlen = max([len(i) for i in traindflist])
+    r = math.ceil(maxlen/64)
+    trainplacelist = [Place(i,replicate=r) for i in traindflist]
+    testplacelist = [Place(testdf, replicate=r)]
+
+    return trainplacelist , testplacelist
